1c8eaeb09a88bac24fe5f69b5d893e6368501f37
Necip Dirik <necipdirik@gmail.com>
2024-04-03 09:40:48 +0300

    handling server-side error 500 added

--- app.js
+++ app.js
@@ -87,6 +87,10 @@ app.get('/confirm', function (req, res) {
 
 app.use(function(req, res) {
   res.render('404');
-})
+});
+
+app.use(function(error, req, res, next) {
+  res.render('500');
+});
 
 app.listen(3000);
