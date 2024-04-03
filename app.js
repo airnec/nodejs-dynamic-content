aea62e4b1f6b7ec311ca544bd94ff6bd7786a9e0
Necip Dirik <necipdirik@gmail.com>
2024-04-03 10:37:54 +0300

    refactoring and importing&exporting added

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const express = require('express');
 const uuid = require('uuid');
 
+const resData = require('./util/restaurant-data');
+
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -22,10 +24,7 @@ app.get('/', function (req, res) {
 app.get('/restaurants', function (req, res) {
   // const htmlFilePath = path.join(__dirname, 'views', 'restaurants.html');
   // res.sendFile(htmlFilePath);
-  const filePath = path.join(__dirname, 'data', 'restaurant.json');
-
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
+  const storedRestaurants = resData.getStoredRestaurants();
 
   res.render('restaurants', {
     numberOfRestaurants: storedRestaurants.length,
@@ -35,10 +34,7 @@ app.get('/restaurants', function (req, res) {
 
 app.get('/restaurants/:id', function (req, res) {
   const restaurantId = req.params.id;
-  const filePath = path.join(__dirname, 'data', 'restaurant.json');
-
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
+  const storedRestaurants = resData.getStoredRestaurants();
 
   for (const restaurant of storedRestaurants) {
     if (restaurant.id === restaurantId) {
@@ -59,14 +55,11 @@ app.get('/recommend', function (req, res) {
 app.post('^/recommend', function (req, res) {
   const restaurant = req.body;
   restaurant.id = uuid.v4();
-  const filePath = path.join(__dirname, 'data', 'restaurant.json');
-
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
+  const restaurants = resData.getStoredRestaurants();
 
   storedRestaurants.push(restaurant);
 
-  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+  resData.storedRestaurants(restaurants);
 
   res.redirect('/confirm');
 });
